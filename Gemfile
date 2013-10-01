fe22d46322483613499778858ae424e025d7bc4e
Matthew Brown <me@matthewbrown.io>
2013-10-01 18:54:34 +0000

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem "rake"
+gem "puppet", ENV['PUPPET_VERSION'] || '~> 3.2.0'
+gem "puppet-lint"
+gem "rspec-puppet"
+gem "puppetlabs_spec_helper"
---
+++ manifests/install.pp
@@ -0,0 +1,44 @@
+class graphite::install {
+
+  include python
+
+  ensure_packages([
+    'python-ldap',
+    'python-cairo',
+    'python-django',
+    'python-twisted',
+    'python-django-tagging',
+    'python-simplejson',
+    'libapache2-mod-python',
+    'python-memcache',
+    'python-pysqlite2',
+    'python-support',
+  ])
+
+  Package['python-pip'] -> Package <| provider == 'pip' and ensure != absent and ensure != purged |>
+
+  package { ['whisper', 'carbon', 'graphite-web']:
+    ensure   => installed,
+    provider => pip,
+    require  => Class['python'],
+  }
+
+  file { '/var/log/carbon':
+    ensure => directory,
+    owner  => www-data,
+    group  => www-data,
+  }
+
+  file {'/var/lib/graphite':
+    ensure => directory,
+    owner  => www-data,
+    group  => www-data,
+  }
+
+  file {'/var/lib/graphite/db.sqlite3':
+    ensure => present,
+    owner  => www-data,
+    group  => www-data,
+  }
+
+}
